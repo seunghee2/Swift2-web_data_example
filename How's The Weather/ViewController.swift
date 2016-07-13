f4b68008beeeaf6409f8d942c1285cf036aca379
seunghee2 <smilagain07@daum.net>
2016-07-13 13:21:42 +0900

    init

---
+++ How's The Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  How's The Weather
+//
+//  Created by 이승희 on 7/13/16.
+//  Copyright © 2016 이승희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
