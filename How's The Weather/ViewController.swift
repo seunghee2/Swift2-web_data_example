7d2526a566b9035f4892935816bfd6933e2449c0
seunghee2 <smilagain07@daum.net>
2016-07-13 14:56:46 +0900

    get web data

--- How's The Weather/ViewController.swift
+++ How's The Weather/ViewController.swift
@@ -14,6 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func findWeather(sender: AnyObject) {
+        
+        let url = NSURL(string: "http://www.weather-forecast.com/locations/" + cityNameTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")!
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            if let urlContent = data {
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                
+                if websiteArray.count > 1 {
+                    let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
+                    
+                    if weatherArray.count > 1 {
+                        let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.resultLabel.text = weatherSummary
+                            print(weatherSummary)
+                        })
+                    }
+                }
+            }
+            
+        }
+        task.resume()
+
     }
     
     override func viewDidLoad() {
