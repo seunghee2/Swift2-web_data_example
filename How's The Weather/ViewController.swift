f4efd050e9b310b1a79522d905246ce3172474dc
seunghee2 <smilagain07@daum.net>
2016-07-13 15:02:37 +0900

    correct the url should be attempted

--- How's The Weather/ViewController.swift
+++ How's The Weather/ViewController.swift
@@ -15,29 +15,39 @@ class ViewController: UIViewController {
     
     @IBAction func findWeather(sender: AnyObject) {
         
-        let url = NSURL(string: "http://www.weather-forecast.com/locations/" + cityNameTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")!
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
-            if let urlContent = data {
-                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+        let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/" + cityNameTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if let url = attemptedUrl {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
                 
-                let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
-                
-                if websiteArray.count > 1 {
-                    let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
+                if let urlContent = data {
                     
-                    if weatherArray.count > 1 {
-                        let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                    let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                    
+                    let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                    
+                    if websiteArray.count > 1 {
                         
-                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                            self.resultLabel.text = weatherSummary
-                            print(weatherSummary)
-                        })
+                        let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
+                        
+                        if weatherArray.count > 1 {
+                            
+                            let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                            
+                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                self.resultLabel.text = weatherSummary
+                                print(weatherSummary)
+                            })
+                            
+                        }
                     }
                 }
+                
             }
-            
+
+            task.resume()
         }
-        task.resume()
 
     }
     
